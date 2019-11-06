158214f0401a1db465677f4d28d80f43a879d476
wenbeir <bweir200@caledonian.ac.uk>
2019-11-06 14:02:52 +0000

    tests used

---
+++ src/test/java/Dec2HexTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+class Dec2HexTest {
+
+    @Test
+    void testMain() {
+        // Setup
+        final String[] args = new String[]{};
+
+        // Run the test
+        Dec2Hex.main(args);
+
+        // Verify the results
+    }
+}
